72bf0e1d3d229e553708b21b0a34712b0cd4a379
maxgerhardt <maximilian.gerhardt@rub.de>
2023-05-23 02:39:43 +0200

    Add more Arduino funcs, correct CAN warning

--- cores/arduino/api/CanMsg.h
+++ cores/arduino/api/CanMsg.h
@@ -68,7 +68,7 @@ public:
     size_t len = 0;
 
     /* Print the header. */
-    len = snprintf(buf, sizeof(buf), "[%08X] (%d) : ", id, data_length);
+    len = snprintf(buf, sizeof(buf), "[%08X] (%d) : ", (unsigned)id, data_length);
     size_t n = p.write(buf, len);
 
     /* Print the data. */
